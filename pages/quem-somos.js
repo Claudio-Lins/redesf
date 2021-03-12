4316a893bf24276863d722de58deffc533d586d9
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-12 23:16:36 +0000

    Update quem-somos.js

--- pages/quem-somos.js
+++ pages/quem-somos.js
@@ -22,7 +22,7 @@ const ConselhoInternacional = () => {
       </div>
       <section>
       <div className="container px-5 py-12 mx-auto">
-      <div className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 text-center">
+      <div className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 text-left border-b">
         <h1>Quem Somos</h1>
       </div>
       <div className="mx-auto text-justify font-light text-gray-600 body-font leading-relaxed">
@@ -51,7 +51,7 @@ const ConselhoInternacional = () => {
         </p>
       </div>
 
-      <div className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900 text-center">
+      <div className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 mt-10 text-left border-b">
         <h1>Missão</h1>
       </div>
       <div className="container mx-auto text-justify font-light text-gray-600 body-font leading-relaxed">
@@ -62,10 +62,10 @@ const ConselhoInternacional = () => {
       </div>
       </section>
       <section className="text-gray-600 body-font">
-        <div className="container px-5 py-12 mx-auto">
-          <div className="flex flex-wrap w-full mb-10 flex-col items-center text-center">
-            <h2 className="sm:text-3xl text-2xl font-medium title-font text-gray-900">Objetivos</h2>
-          </div>
+        <div className="container px-5 py-2 mx-auto">
+        <div className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900 text-left border-b">
+        <h1>Objetivos</h1>
+      </div>
           <div className="flex flex-wrap -m-4">
             <div className="xl:w-1/3 md:w-1/2 p-4">
               <div className="border flex items-center border-gray-200 p-6 rounded-lg">
