5411c904660a01a75a165974681e2f891f870502
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-14 11:00:23 +0000

    upgrade - cons. inter

--- pages/conselho-internacional.js
+++ pages/conselho-internacional.js
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import Head from 'next/head'
+import Head from "next/head";
 
 const ConselhoInternacional = () => {
   return (
-    
-    <div className="min-h-screen bg-gray-50 mt-28 mb-20">
+    <div className="min-h-screen bg-white mt-36 mb-20 container mx-auto">
       <Head>
         <title>RedeSF - Conselho Internacional</title>
         <link rel="icon" href="/favicon.ico" />
@@ -19,14 +18,19 @@ const ConselhoInternacional = () => {
       <div className="lg:flex p-8 lg:items-center md:px-48 lg:py-2">
         <div className="flex-grow bg-white shadow-lg rounded-md border p-6">
           <div className="sm:flex sm:items-center sm:mx-auto">
-            <img
-              src="/conselho-internacional/mocha.png"
-              className="sm:flex-shrink-0 sm:mx-0 h-24 mx-auto mb-4"
-            />
+            <div className=" flex-shrink-0 mb-2 flex justify-center sm:items-center">
+              <Image
+                className="rounded-full object-cover"
+                src="/quem-somos/conselho-internacional/image09.jpeg"
+                alt="Carolina Huatay"
+                width={80}
+                height={80}
+              />
+            </div>
             <div className="sm:ml-4 sm:text-left text-center">
-              <h4 className="">
+            <h4 className="">
                 <strong className="text-sm text-gray-800">
-                  Presidenta: Aida Garcia Naranjo | 
+                  Presidenta: Aida Garcia Naranjo |
                 </strong>
                 <a
                   className="text-xs  ml-2 text-blue-400"
@@ -36,7 +40,7 @@ const ConselhoInternacional = () => {
                 </a>
               </h4>
               <p className="text-gray-600 text-sm font-light text-justify">
-                Educadora, cientista política, pesquisadora social e política,
+              Educadora, cientista política, pesquisadora social e política,
                 especialista em gênero, em captação de recursos multilaterais e
                 bilaterais, além de ser promotora cultural. Foi Embaixadora do
                 Peru na República Oriental do Uruguai, representante do Peru na
@@ -48,157 +52,261 @@ const ConselhoInternacional = () => {
           </div>
         </div>
       </div>
+      
+      <div className=' container px-4 sm:px-48 sm:py-2'><hr></hr></div>
+
       <div className="lg:flex p-8 lg:items-center md:px-48 lg:py-2">
         <div className="flex-grow bg-white shadow-lg rounded-md border p-6">
           <div className="sm:flex sm:items-center sm:mx-auto">
-            <img
-              src="/conselho-internacional/Valeria-Chiavetta.png"
-              className="sm:flex-shrink-0 sm:mx-0 h-24 mx-auto mb-4"
-            />
+            <div className=" flex-shrink-0 mb-2 flex justify-center sm:items-center">
+              <Image
+                className="rounded-full object-cover"
+                src="/quem-somos/conselho-internacional/image08.jpeg"
+                alt="Carolina Huatay"
+                width={80}
+                height={80}
+              />
+            </div>
             <div className="sm:ml-4 sm:text-left text-center">
-              <h4 className="">
+            <h4 className="">
                 <strong className="text-sm text-gray-800">
-                Valeria Chiavetta | 
+                  Valeria Chiavetta |
                 </strong>
                 <a
                   className="text-xs ml-2 text-blue-400"
                   href="mailto:v.chiavetta@redesf.org"
                 >
-                v.chiavetta@redesf.org 
+                  v.chiavetta@redesf.org
                 </a>
               </h4>
               <p className="text-gray-600 text-sm font-light text-justify">
-              Licenciada em Serviço Social, professora no Departamento de Práticas de Serviço Social e docente da Cátedra. Eletiva de Direitos Humanos da Faculdade de Ciências Políticas e Sociais da Universidade Nacional de Cuyo, em Mendoza, na Argentina. É presidenta da Associação Ecumênica de Cuyo, coordenadora do Projeto Migrantes da Associação Ecumênica de Cuyo e coordenadora nacional argentina do Programa Mercosul Social e Solidário (PMSS).
+              Licenciada em Serviço Social, professora no Departamento de
+                Práticas de Serviço Social e docente da Cátedra. Eletiva de
+                Direitos Humanos da Faculdade de Ciências Políticas e Sociais da
+                Universidade Nacional de Cuyo, em Mendoza, na Argentina. É
+                presidenta da Associação Ecumênica de Cuyo, coordenadora do
+                Projeto Migrantes da Associação Ecumênica de Cuyo e coordenadora
+                nacional argentina do Programa Mercosul Social e Solidário
+                (PMSS).
               </p>
             </div>
           </div>
         </div>
       </div>
+      
+      <div className=' container px-4 sm:px-48 sm:py-2'><hr></hr></div>
+
       <div className="lg:flex p-8 lg:items-center md:px-48 lg:py-2">
         <div className="flex-grow bg-white shadow-lg rounded-md border p-6">
           <div className="sm:flex sm:items-center sm:mx-auto">
-            <img
-              src="/conselho-internacional/Patricia-Gainza.png"
-              className="sm:flex-shrink-0 sm:mx-0 h-24 mx-auto mb-4"
-            />
+            <div className=" flex-shrink-0 mb-2 flex justify-center sm:items-center">
+              <Image
+                className="rounded-full object-cover"
+                src="/quem-somos/conselho-internacional/image03.jpeg"
+                alt="Carolina Huatay"
+                width={80}
+                height={80}
+              />
+            </div>
             <div className="sm:ml-4 sm:text-left text-center">
-              <h4 className="">
+            <h4 className="">
                 <strong className="text-sm text-gray-800">
-                Patricia Gainza | 
+                  Patricia Gainza |
                 </strong>
                 <a
                   className="text-xs ml-2 text-blue-400"
                   href="mailto:p.gainza@redesf.org"
                 >
-                p.gainza@redesf.org 
+                  p.gainza@redesf.org
                 </a>
               </h4>
               <p className="text-gray-600 text-sm font-light text-justify">
-              Mestre em Sociologia com especialização em Integração, Mercosul e Desenvolvimento, exclusão social e redução da pobreza. Atualmente é diretora da Divisão de Direitos Humanos da Direção Nacional de Promoção Sociocultural, Ministério do Desenvolvimento Social (MIDES), em Montevideo, no Uruguai.
+              Mestre em Sociologia com especialização em Integração, Mercosul
+                e Desenvolvimento, exclusão social e redução da pobreza.
+                Atualmente é diretora da Divisão de Direitos Humanos da Direção
+                Nacional de Promoção Sociocultural, Ministério do
+                Desenvolvimento Social (MIDES), em Montevideo, no Uruguai.
               </p>
             </div>
           </div>
         </div>
       </div>
+
+      <div className=' container px-4 sm:px-48 sm:py-2'><hr></hr></div>
+      
       <div className="lg:flex p-8 lg:items-center md:px-48 lg:py-2">
         <div className="flex-grow bg-white shadow-lg rounded-md border p-6">
           <div className="sm:flex sm:items-center sm:mx-auto">
-            <img
-              src="/conselho-internacional/Alfonso-Ramiro.jpg"
-              className="sm:flex-shrink-0 sm:mx-0 h-24 mx-auto mb-4"
-            />
+            <div className=" flex-shrink-0 mb-2 flex justify-center sm:items-center">
+              <Image
+                className="rounded-full object-cover"
+                src="/quem-somos/conselho-internacional/image04.jpeg"
+                alt="Carolina Huatay"
+                width={80}
+                height={80}
+              />
+            </div>
             <div className="sm:ml-4 sm:text-left text-center">
-              <h4 className="">
+            <h4 className="">
                 <strong className="text-sm text-gray-800">
-                Alfonso Ramiro Hinojosa Gordonava | 
+                  Alfonso Ramiro Hinojosa Gordonava |
                 </strong>
                 <a
                   className="text-xs ml-2 text-blue-400"
                   href="mailto:a.hinojosa@redesf.org"
                 >
-                a.hinojosa@redesf.org 
+                  a.hinojosa@redesf.org
                 </a>
               </h4>
               <p className="text-gray-600 text-sm font-light text-justify">
-              Sociólogo, mestre em Ciências Sociais com especialidade em Antropologia. Membro fundador da Rede Sem Fronteiras. Membro do Grupo de Trabalho sobre Migração e Cultura CLACSO (Conselho Latinoamericano de Ciências Sociais). Professor e pesquisador convidado do Instituto de Pesquisa, Interação e Pós-graduação da Carreira de Serviço Social da UMSA. Pesquisador da temática migratória para instituições como PIEB (Programa de Investigação Estratégica de Bolivia), CESU (Centro Universitário de Estudos Superiores), defensor do povo. Membro do CEDLA (Centro de Estudos para o Desenvolvimento do Trabalho e Agrário), Coordenador da Mulher, Oxfam Bolívia e da Fundação pela Paz.
+              Sociólogo, mestre em Ciências Sociais com especialidade em
+                Antropologia. Membro fundador da Rede Sem Fronteiras. Membro do
+                Grupo de Trabalho sobre Migração e Cultura CLACSO (Conselho
+                Latinoamericano de Ciências Sociais). Professor e pesquisador
+                convidado do Instituto de Pesquisa, Interação e Pós-graduação da
+                Carreira de Serviço Social da UMSA. Pesquisador da temática
+                migratória para instituições como PIEB (Programa de Investigação
+                Estratégica de Bolivia), CESU (Centro Universitário de Estudos
+                Superiores), defensor do povo. Membro do CEDLA (Centro de
+                Estudos para o Desenvolvimento do Trabalho e Agrário),
+                Coordenador da Mulher, Oxfam Bolívia e da Fundação pela Paz.
               </p>
             </div>
           </div>
         </div>
       </div>
+
+      <div className=' container px-4 sm:px-48 sm:py-2'><hr></hr></div>
+
       <div className="lg:flex p-8 lg:items-center md:px-48 lg:py-2">
         <div className="flex-grow bg-white shadow-lg rounded-md border p-6">
           <div className="sm:flex sm:items-center sm:mx-auto">
-            <img
-              src="/conselho-internacional/Kattya-Gonzalez.jpg"
-              className="sm:flex-shrink-0 sm:mx-0 h-24 mx-auto mb-4"
-            />
+            <div className=" flex-shrink-0 mb-2 flex justify-center sm:items-center">
+              <Image
+                className="rounded-full object-cover"
+                src="/quem-somos/conselho-internacional/image05.jpeg"
+                alt="Carolina Huatay"
+                width={80}
+                height={80}
+              />
+            </div>
             <div className="sm:ml-4 sm:text-left text-center">
-              <h4 className="">
+            <h4 className="">
                 <strong className="text-sm text-gray-800">
-                Kattya Gonzalez | 
+                  Kattya Gonzalez |
                 </strong>
                 <a
                   className="text-xs ml-2 text-blue-400"
                   href="mailto:k.gonzalez@redesf.org"
                 >
-                k.gonzalez@redesf.org 
+                  k.gonzalez@redesf.org
                 </a>
               </h4>
               <p className="text-gray-600 text-sm font-light text-justify">
-              Pós graduada em Direito Penal e Processo Penal. Reitora da Universidade Nacional de Assunção, no Paraguai. É autora dos livros “Garantías Constitucionales en el Constitucionalismo Iberoamericano”, escrito na Universidade Complutense de Madri – Espanha e “Reforma Constitucional. Debate Anterior. Corrupção no Supremo Tribunal de Justiça”. Também é professora de Direito Agrário e Direito do Trabalho na Universidade de Ciências Jurídicas da U.C.A. Experiência profissional com ênfase em direito público. Ex-vereadora Municipal do Distrito de Villa Elisa entre 2006 e 2010. Presidenta da Coordenadoria de Advogados do Paraguai.
+              Pós graduada em Direito Penal e Processo Penal. Reitora da
+                Universidade Nacional de Assunção, no Paraguai. É autora dos
+                livros “Garantías Constitucionales en el Constitucionalismo
+                Iberoamericano”, escrito na Universidade Complutense de Madri –
+                Espanha e “Reforma Constitucional. Debate Anterior. Corrupção no
+                Supremo Tribunal de Justiça”. Também é professora de Direito
+                Agrário e Direito do Trabalho na Universidade de Ciências
+                Jurídicas da U.C.A. Experiência profissional com ênfase em
+                direito público. Ex-vereadora Municipal do Distrito de Villa
+                Elisa entre 2006 e 2010. Presidenta da Coordenadoria de
+                Advogados do Paraguai.
               </p>
             </div>
           </div>
         </div>
       </div>
+      
+      <div className=' container px-4 sm:px-48 sm:py-2'><hr></hr></div>
+
       <div className="lg:flex p-8 lg:items-center md:px-48 lg:py-2">
         <div className="flex-grow bg-white shadow-lg rounded-md border p-6">
           <div className="sm:flex sm:items-center sm:mx-auto">
-            <img
-              src="/conselho-internacional/Carolina-Huatay.jpg"
-              className="sm:flex-shrink-0 sm:mx-0 h-24 mx-auto mb-4"
-            />
+            <div className=" flex-shrink-0 mb-2 flex justify-center sm:items-center">
+              <Image
+                className="rounded-full object-cover"
+                src="/quem-somos/conselho-internacional/image06.jpeg"
+                alt="Carolina Huatay"
+                width={80}
+                height={80}
+              />
+            </div>
             <div className="sm:ml-4 sm:text-left text-center">
               <h4 className="">
                 <strong className="text-sm text-gray-800">
-                Carolina Huatay | 
+                Carolina Huatay |
                 </strong>
                 <a
                   className="text-xs ml-2 text-blue-400"
                   href="mailto:c.huatay@redesf.org"
                 >
-                c.huatay@redesf.org 
+                  c.huatay@redesf.org
                 </a>
               </h4>
               <p className="text-gray-600 text-sm font-light text-justify">
-              Comunicadora Social com habilitação em jornalismo no Peru, possui especialização em migração, cidadania, direitos humanos, direitos ESC e políticas públicas do Brasil, Chile, Costa Rica e México. Foi Coordenadora do Programa de Gênero do Observatório Interamericano de Migrações, Coordenadora do Programa Social para Migrantes do Consulado do Peru, em Santiago do Chile, Coordenadora do Programa de Migrantes do Município de Recoleta, Chefe da Unidade de Apoio à Família do Ministério do Desenvolvimento Social, com diversos projetos de migração e integração para o Ministério da Educação e várias Intendências, Universidades e RSE. Ela também publicou vários artigos sobre gênero, redes e cidadania, entre outras, em vários países. Atualmente é coordenadora do Programa Andino de Dignidade Humana – PROANDES, Chile Capítulo do Grito dos Excluídos e AssessoraTécnica do Escritório do Migrante da Prefeitura de Santiago.
+              Comunicadora Social com habilitação em jornalismo no Peru,
+                possui especialização em migração, cidadania, direitos humanos,
+                direitos ESC e políticas públicas do Brasil, Chile, Costa Rica e
+                México. Foi Coordenadora do Programa de Gênero do Observatório
+                Interamericano de Migrações, Coordenadora do Programa Social
+                para Migrantes do Consulado do Peru, em Santiago do Chile,
+                Coordenadora do Programa de Migrantes do Município de Recoleta,
+                Chefe da Unidade de Apoio à Família do Ministério do
+                Desenvolvimento Social, com diversos projetos de migração e
+                integração para o Ministério da Educação e várias Intendências,
+                Universidades e RSE. Ela também publicou vários artigos sobre
+                gênero, redes e cidadania, entre outras, em vários países.
+                Atualmente é coordenadora do Programa Andino de Dignidade Humana
+                – PROANDES, Chile Capítulo do Grito dos Excluídos e
+                AssessoraTécnica do Escritório do Migrante da Prefeitura de
+                Santiago.
               </p>
             </div>
           </div>
         </div>
       </div>
+
+      <div className=' container px-4 sm:px-48 sm:py-2'><hr></hr></div>
+
       <div className="lg:flex p-8 lg:items-center md:px-48 lg:py-2">
         <div className="flex-grow bg-white shadow-lg rounded-md border p-6">
           <div className="sm:flex sm:items-center sm:mx-auto">
-            <img
-              src="/conselho-internacional/Valeria-Espana.jpg"
-              className="sm:flex-shrink-0 sm:mx-0 h-16 mx-auto mb-4 rounded-full"
-            />
+            <div className=" flex-shrink-0 mb-2 flex justify-center sm:items-center">
+              <Image
+                className="rounded-full object-cover"
+                src="/quem-somos/conselho-internacional/image07.jpeg"
+                alt="Valeria España"
+                width={80}
+                height={80}
+              />
+            </div>
             <div className="sm:ml-4 sm:text-left text-center">
               <h4 className="">
                 <strong className="text-sm text-gray-800">
-                Valeria España | 
+                  Valeria España |
                 </strong>
                 <a
                   className="text-xs ml-2 text-blue-400"
                   href="mailto:v.espanha@redesf.org"
                 >
-                v.espanha@redesf.org 
+                  v.espanha@redesf.org
                 </a>
               </h4>
               <p className="text-gray-600 text-sm font-light text-justify">
-              Advogada pela Universidade Nacional Autônoma do México, Mestre em Direitos Humanos e Políticas Públicas pela Universidade Nacional de Lanús em Buenos Aires, na Argentina, e doutoranda pela mesma universidade. É professora convidada no Departamento de Direitos Humanos e na cadeira opcional de Direito e Gênero, é colunista do jornal La Diaria e membro do Conselho Consultivo da Suplemento Incorrecta. Trabalhou como consultora externa para diversos organismos internacionais, como o Instituto de Políticas Públicas e Direitos Humanos do Mercosul, a Comissão Interamericana de Direitos Humanos e a Organização Internacional para as Migrações, entre outros.
+                Advogada pela Universidade Nacional Autônoma do México, Mestre
+                em Direitos Humanos e Políticas Públicas pela Universidade
+                Nacional de Lanús em Buenos Aires, na Argentina, e doutoranda
+                pela mesma universidade. É professora convidada no Departamento
+                de Direitos Humanos e na cadeira opcional de Direito e Gênero, é
+                colunista do jornal La Diaria e membro do Conselho Consultivo da
+                Suplemento Incorrecta. Trabalhou como consultora externa para
+                diversos organismos internacionais, como o Instituto de
+                Políticas Públicas e Direitos Humanos do Mercosul, a Comissão
+                Interamericana de Direitos Humanos e a Organização Internacional
+                para as Migrações, entre outros.
               </p>
             </div>
           </div>
