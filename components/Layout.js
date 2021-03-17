a65e13bc72c7cc4b833ba888ab0d73df3470bd9b
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-17 15:23:59 +0000

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import Nav from "./Nav"
 
 const Layout = ({ children }) => {
     return (
-        <div>
+        <div className="antialiased text-gray-900 flex items-center justify-center min-h-screen">
             <Nav/>
             { children }
         </div>
