8805681f5463c13c33586574f64cc5a159d5b364
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-23 18:14:27 +0000

    Revert "install Strapi"
    This reverts commit a9fc1152cd59d4df1fe66c2b5defa17c18366ba7.

--- blog-strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '1191e2a4cc8b140d9d5871f455599e50'),
-    },
-  },
-});
