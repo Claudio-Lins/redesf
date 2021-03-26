aced180ed649f3831d274a1fed09e90bac2c9316
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-26 21:52:58 +0000

    .

--- pages/quem-somos.js
+++ pages/quem-somos.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Head from "next/head";
-import LogosMembros from '../utils/LogosMembros'
+//import LogosMembros from '../utils/LogosMembros'
 
 
 export default function About( {aboutPage} ) {
@@ -134,11 +134,181 @@ export default function About( {aboutPage} ) {
             width={1920}
             height={480}
           />
-          <div className='md:px-20 mt-6'>
+          {/* <div className='md:px-20 mt-6'>
           <LogosMembros/>
-          </div>
+          </div> */}
+
+
+
         </div>
       </section>
+
+      <section className="container">
+      <div className="mx-auto grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 text-center space-y-2 items-center">
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/organizacao-filhos-do-mundo_lg.png"
+            alt="Organização Filhos do Mundo"
+            width={90}
+            height={90}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/cdh-lg-200x142.png"
+            alt="CDH"
+            width={100}
+            height={71}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/casa-do-brasil-de-lisboa-lg.png"
+            alt="Casa do Brasil de Lisboa"
+            width={90}
+            height={90}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/cdhic-color-horizontal-espacio-sin-fronteras-esf-100x50.png"
+            alt="CDHIC"
+            width={100}
+            height={50}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/proandes-lg-100x100.jpg"
+            alt="Proandes"
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/unifesp-lindomar-albuquerque-lg-166x100.png"
+            alt="Unifesp"
+            width={133}
+            height={80}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/cedal-lg-100x100.png"
+            alt="Cedal"
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/fec-lg-100x100.jpg"
+            alt="FEC"
+            width={80}
+            height={80}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/del-agora-lg.jpg"
+            alt="Del"
+            width={166}
+            height={100}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/codhes-lg-144x50.png"
+            alt="Codhes"
+            width={144}
+            height={50}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/coapy-lg-113x80.jpg"
+            alt="Coapy"
+            width={135}
+            height={96}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/re-lg-159x70.jpg"
+            alt="Del"
+            width={159}
+            height={70}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/pal-oriana-Jara-lg-113x80.png"
+            alt="Pal Oriana Jara"
+            width={113}
+            height={80}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/coordenacao-direitos-sociais-Saude-e-seguridade-lg-120x85.jpg"
+            alt="Coordenação Direitos Sociais Saude e Seguridade"
+            width={120}
+            height={85}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/alampyme-br-360x70.gif"
+            alt="Del"
+            width={288}
+            height={56}
+          />
+        </div>
+        <div className="p-4 hidden sm:flex">
+          <Image
+            src="/quem-somos/logos/image-blank-75x150.png"
+            alt="image-blanck"
+            width={75}
+            height={150}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/umsa-lg-74x150.png"
+            alt="Codhes"
+            width={74}
+            height={150}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/Instituto-edesio-passos-lg-113x80.png"
+            alt="Coapy"
+            width={113}
+            height={80}
+          />
+        </div>
+        <div className="p-4">
+          <Image
+            src="/quem-somos/Logos/casa-da-gente-lg-80x120.png"
+            alt="Casa da Gente"
+            width={80}
+            height={120}
+          />
+        </div>
+
+        <div className="p-4 hidden sm:flex">
+          <Image
+            src="/quem-somos/logos/image-blank-75x150.png"
+            alt="image-blank"
+            width={80}
+            height={120}
+          />
+        </div>
+      </div>
+    </section>
+
     </div>
 );
 }
