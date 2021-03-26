5e83e4a3245a5b6ac095770ae8d793dae8c398d0
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-26 12:43:01 +0000

    Update teste-api.js

--- pages/teste-api.js
+++ pages/teste-api.js
@@ -1,19 +1,27 @@
-import fetch from 'isomorphic-unfetch'
+import { useEffect } from "react";
 
-export default function Teste(props) {
-    return (
-   
-        <div className='py-16'>{props.posts.title}</div>
-    );
+export default function Teste({ conselhoInternacional }) {
+  return (
+    <div className='py-20'>
+      {conselhoInternacional && conselhoInternacional.map((conselho) => (
+
+        <div key={conselho.id}>
+          <h2>{conselho.email}</h2>
+        </div>
+      ))}
+    </div>
+  );
 }
- 
 
-export async function getStaticProps(context) {
-    const postsRes = await fetch('https://cryptic-retreat-90035.herokuapp.com/posts')
-    const posts = await postsRes.json();
-    return {
-      props: {
-        posts,
-      },
-    }
+export async function getStaticProps() {
+  const res = await fetch(
+    "https://cryptic-retreat-90035.herokuapp.com/conselho-internacionals"
+  );
+  const conselhoInternacional = await res.json();
+
+  return {
+    props: {
+      conselhoInternacional,
+    },
+  };
 }
