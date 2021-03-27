402c7f7643b6c6c0d782f634af36fce4b884e8ca
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-03-27 23:02:08 +0000

    Update single-post.js

--- pages/single-post.js
+++ pages/single-post.js
@@ -9,7 +9,7 @@ export default function Post() {
         // {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
 
-      <section className="bg-purple-800 p-6 md:py-20">
+      <section className="bg-purple-800 py-12 px-4 md:py-20">
         <div className="md:w-1/3 mx-auto">
           <div className="text-gray-50 text-3xl font-semibold text-center">
             <h1>
@@ -23,18 +23,18 @@ export default function Post() {
           </div>
         </div>
       </section>
-      <section className="bg-gray-50 rounded-lg shadow-lg md:w-2/5 md:-mt-10 mx-auto">
+      <section className="bg-gray-50 rounded-xl shadow-lg py-2 md:w-2/5 md:-mt-10 mx-auto">
         <div className="text-center px-4">
-          <div className="rounded-lg p-2">
+          <div className="rounded-lg p-2 -mt-10 md:mt-0">
             <Image
-              className="rounded-md shadow-lg text-center"
+              className="rounded-xl shadow-xl text-center"
               src="/blog/blog-cover.jpg"
               alt="Blog - Rede Sem Fronteiras"
               width={680}
               height={450}
             />
           </div>
-          <div className='text-sm font-light text-gray-700 p-4'>
+          <div className='text-sm font-light text-gray-700 px-4 py-2 md:p-8 tracking-wider text-justify'>
             <p>
               Em assembleia, participantes reuniram-se por dois dias para
               debater e priorizar as propostas geradas ao longo das atividades
